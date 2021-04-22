b28923f7a1ec5297bcb175e8da1a7c924132514d
Godswilldev <gimmex1@gmail.com>
2021-04-22 10:04:18 +0100

    used a different api to get the user's ip address

--- script/app.js
+++ script/app.js
@@ -1,37 +1,8 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-
 const IpAddresshtml = document.querySelector(".IpAddress");
 const locationHtml = document.querySelector(".location");
 const timezoneHtml = document.querySelector(".timezone");
 const ispHtml = document.querySelector(".isp");
-
-const ipAddressUrl = "https://api.ipify.org?format=json";
-
-///////////////////getting the Ip address on page load and displaying it
-(async function getIpAddress() {
-  try {
-    const { data: IpAddressGotten } = await axios.get(ipAddressUrl);
-    console.log(IpAddressGotten.ip);
-    const api_key = "at_cKo17Jw64ZK7HmrIuVHmwufMSTOnm";
-    const geoIpAddess = `https://geo.ipify.org/api/v1?apiKey=${api_key}&ipAddress=${IpAddressGotten.ip}`;
-
-    const { data: ipdetails } = await axios.get(geoIpAddess);
-    console.log(ipdetails);
-
-    const { city, country, lat, lng, region, timezone } = ipdetails.location;
-    console.log(city, country, lat, lng, region, timezone);
-
-    IpAddresshtml.textContent = IpAddressGotten.ip;
-    locationHtml.innerHTML = `<h1 class="heading__secondary result__value location">${city} <br /> ${region}, <br />${country}</h1>`;
-    timezoneHtml.textContent = timezone;
-    ispHtml.textContent = ipdetails.isp;
-  } catch (error) {
-    console.log(error);
-  }
-})();
 
 ////////////// getting the location of the user to display the map
 ////////////////////////////////////////////////////////////////////
@@ -40,9 +11,7 @@ navigator.geolocation.getCurrentPosition(
   (position) => {
     const { latitude: lat } = position.coords;
     const { longitude: lng } = position.coords;
-    console.log(`https://www.google.com/maps/@${lat},${lng}`);
     let coords = [lat, lng];
-    console.log(lat, lng);
 
     var map = L.map("map").setView(coords, 7);
 
@@ -56,3 +25,33 @@ navigator.geolocation.getCurrentPosition(
     console.log("couldn't get location");
   }
 );
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////getting the Ip address on page load and displaying it
+///////////////////////////////////////////////////////////////////////////////
+
+const ipAddressUrl = "https://ipinfo.io/json?token=f958fc223af431";
+(async function getIpAddress() {
+  try {
+    const { data: IpAddressGotten } = await axios.get(ipAddressUrl);
+    const api_key = "at_cKo17Jw64ZK7HmrIuVHmwufMSTOnm";
+    const geoIpAddess = `https://geo.ipify.org/api/v1?apiKey=${api_key}&ipAddress=${IpAddressGotten.ip}`;
+
+    const { data: ipdetails } = await axios.get(geoIpAddess);
+
+    const { city, country, lat, lng, region, timezone } = ipdetails.location;
+
+    IpAddresshtml.textContent = IpAddressGotten.ip;
+    locationHtml.innerHTML = `<h1 class="heading__secondary result__value location">${city} <br /> ${region}, <br />${country}</h1>`;
+    timezoneHtml.textContent = timezone;
+    ispHtml.textContent = ipdetails.isp;
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+////////////////////////////////
+//handling form submission event
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
